5747a91362e7bbb2ffdab9e59562167ee0635a8f
Dustin Flanary <singflan@gmail.com>
2017-02-11 12:57:48 -0700

    Finished Strings section

---
+++ color.rb
@@ -0,0 +1,13 @@
+colors = 'blue pink yellow orange'
+
+if colors.include?('yellow')
+  puts true
+else
+  puts false
+end
+
+if colors.include?('purple')
+  puts true
+else 
+  puts false
+end
---
+++ pluralize.rb
@@ -0,0 +1,7 @@
+words = 'car human elephant airplane'
+
+words_arr = words.split(" ")
+
+words_arr.each do |word|
+  puts word + 's'
+end
